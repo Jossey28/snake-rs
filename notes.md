488361d12b614b7f267d3d239ca4e08f782a3dfd
Irene A <121013355+Jossey28@users.noreply.github.com>
2026-05-04 20:47:56 -0500

    Ratatui Counter example from website. Last tut before starting snake

---
+++ notes.md
@@ -0,0 +1,10 @@
+# Things that sound important enough to note down
+
+## Ratatui app composition
+from: https://ratatui.rs/tutorials/counter-app/basic-app/
+
+A common pattern found in most apps are:
+ - Init the terminal
+ - Run the app in a game loop
+ - Restore the terminal back to its original state
+
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,105 @@
-// use color_eyre::eyre::Ok;
-use ratatui::{DefaultTerminal, Frame, crossterm};
+use std::io;
 
-fn main() -> color_eyre::Result<()> {
-    color_eyre::install()?;
-    ratatui::run(app)?;
-    Ok(())
+use crossterm::{event::{self, Event, KeyCode, KeyEvent, KeyEventKind}, terminal};
+use ratatui::{
+    buffer::Buffer,
+    layout::Rect,
+    style::Stylize,
+    symbols::border,
+    text::{Line, Text},
+    widgets::{Block, Paragraph, Widget},
+    DefaultTerminal, 
+    Frame, 
+};
+
+fn main() -> io::Result<()> {
+    ratatui::run(|terminal| App::default().run(terminal))
 }
 
-fn app(terminal: &mut DefaultTerminal) -> std::io::Result<()> {
-    loop {
-        terminal.draw(render)?;
-        if crossterm::event::read()?.is_key_press() {
-            break Ok(());
-        }s
+#[derive(Debug)]
+pub struct App {
+    counter: u8,
+    exit: bool,
+}
+
+impl Default for App {
+    fn default() -> Self {
+        Self { counter: 0, exit: false }
     }
 }
 
-fn render(frame: &mut Frame) {
-    frame.render_widget("Hello World", frame.area());
+impl App {
+    pub fn run(&mut self, terminal: &mut DefaultTerminal) -> io::Result<()> {
+        while !self.exit {
+            terminal.draw(|frame| self.draw(frame))?;
+            self.handle_events()?;
+        }
+        Ok(())
+    }
+
+    fn exit(&mut self) {
+        self.exit = true;
+    }
+
+    fn increment_counter(&mut self) {
+        self.counter += 1;
+    }
+
+    fn decrement_counter(&mut self) {
+        if self.counter > 0 {
+            self.counter -=1;
+        }   
+    }
+
+    fn draw(&self, frame: &mut Frame) {
+        frame.render_widget(self, frame.area());
+    }
+
+    fn handle_events(&mut self) -> io::Result<()> {
+        match event::read()? {
+            Event::Key(key_event) if key_event.kind == KeyEventKind::Press => {
+                self.handle_key_event(key_event)
+            }
+            _ => {}
+        }
+        Ok(())
+    }
+
+    fn handle_key_event(&mut self, key_event: KeyEvent) {
+        match key_event.code {
+            KeyCode::Char('q') => self.exit(),
+            KeyCode::Left | KeyCode::Char('a') => self.decrement_counter(),
+            KeyCode::Right | KeyCode::Char('d') => self.increment_counter(),
+            _ => {}
+        }
+    }
 }
+
+impl Widget for &App {
+    fn render(self, area: Rect, buf: &mut Buffer) {
+        let title = Line::from(" Rataui Counter Tut ".bold());
+        let instructions = Line::from(vec![
+            " Decrement ".into(),
+            "<Left>".blue().bold(),
+            " Increment ".into(),
+            "<Right>".blue().bold(),
+            " Quit ".into(),
+            "<Q> ".blue().bold(),
+        ]);
+
+        let block = Block::bordered()
+            .title(title.centered())
+            .title_bottom(instructions.centered())
+            .border_set(border::THICK);
+
+        let counter_text = Text::from(vec![Line::from(vec![
+            "Value: ".into(),
+            self.counter.to_string().yellow(),
+        ])]);
+
+        Paragraph::new(counter_text)
+            .centered()
+            .block(block)
+            .render(area, buf);
+        }
+    }
